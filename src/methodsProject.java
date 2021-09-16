6f82086f923ca2a2e48fe0dd518c00a45f02e21f
Emil Tølbøll <eto@aganalytics.dk>
2021-09-17 00:26:26 +0200

    second commit

--- src/methodsProject.java
+++ src/methodsProject.java
@@ -10,10 +10,26 @@ public class methodsProject {
         //double lowestNumber = numbersArray[0];
         return numbersArray[0];
     }
+
+    public static double getTriangleArea(double side1,double side2,double side3){
+        double semiParameter = (side1+side2+side3)/2.;
+        return Math.sqrt(semiParameter*(semiParameter-side1)*(semiParameter-side2)*(semiParameter-side3));
+    }
+
     public static char getMiddleCharacter(String str){
         int lengthOfString = str.length();
         //double lengthOfStringDouble = lengthOfString;
         return str.charAt((int)(lengthOfString/2.-1));
+    }
+
+    public static boolean isPasswordValid(String str){
+        boolean check1 = str.length()>7;
+        boolean check2 = str.matches("[a-zA-Z0-9]*");
+        boolean check3 = !str.contains("secret");
+        boolean check4 = str.charAt(0)!='-';
+        boolean[] booleanArray = {check1,check2,check3,check4};
+        for(boolean b : booleanArray) if(!b) return false;
+        return true;
     }
 
     public static void getSignOfNumber(double inputNumber){
@@ -28,9 +44,16 @@ public class methodsProject {
         }
     }
 
+    public static void printNumbers(int maximumNumber){
+        for (int i = 0;i<maximumNumber;i++){
+            System.out.print("[" + (i+1) + "]");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Task 1!");
         System.out.println("Input the first number:");
         double num1 = sc.nextDouble();
         System.out.println("Input the second number:");
@@ -44,11 +67,13 @@ public class methodsProject {
 
         System.out.println("The lowest number is " + getLowestNumber(numberArray));
 
+        System.out.println("Task 2!");
         double inputNumber;
         System.out.println("Enter number:");
         inputNumber = sc.nextDouble();
         getSignOfNumber(inputNumber);
 
+        System.out.println("Task 3!");
 
         char middleCharacter = getMiddleCharacter("benjamin");
         System.out.println(middleCharacter);
@@ -56,6 +81,24 @@ public class methodsProject {
 
 
 
+        System.out.println("Task 4!");
+        System.out.println("Input side-1:");
+        double side1 = sc.nextDouble();
+        System.out.println("Input side-2:");
+        double side2 = sc.nextDouble();
+        System.out.println("Input side-3:");
+        double side3 = sc.nextDouble();
+        System.out.println("The area of the triangle is " + getTriangleArea(side1,side2,side3));
 
+        System.out.println("Task 5!");
+        System.out.println("Please enter password:");
+        String pwString = sc.next();
+        System.out.println("Is password valid? " + isPasswordValid(pwString));
+
+        System.out.println("Task 6!");
+
+        System.out.println("Enter maximum number:");
+        int maxNumber = sc.nextInt();
+        printNumbers(maxNumber);
     }
 }
